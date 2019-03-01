5b2f2545b05dcae0c232b4c7248d806f8cfcdea5
anoop-ra <anoopkollam97@gmail.com>
2019-03-01 20:11:57 +0530

    .

---
+++ SLAM/hector_slam_test/build/RPLidar_Hector_SLAM/hector_slam/hector_geotiff/CMakeFiles/geotiff_saver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geotiff_saver.dir/src/geotiff_saver.cpp.o"
+  "/home/anoop/Desktop/hector_slam_test/devel/lib/hector_geotiff/geotiff_saver.pdb"
+  "/home/anoop/Desktop/hector_slam_test/devel/lib/hector_geotiff/geotiff_saver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geotiff_saver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SLAM/hector_slam_test/build/RPLidar_Hector_SLAM/hector_slam/hector_mapping/CMakeFiles/hector_mapping_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hector_mapping_generate_messages_py"
+  "/home/anoop/Desktop/hector_slam_test/devel/lib/python2.7/dist-packages/hector_mapping/msg/_HectorDebugInfo.py"
+  "/home/anoop/Desktop/hector_slam_test/devel/lib/python2.7/dist-packages/hector_mapping/msg/_HectorIterData.py"
+  "/home/anoop/Desktop/hector_slam_test/devel/lib/python2.7/dist-packages/hector_mapping/msg/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/hector_mapping_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
